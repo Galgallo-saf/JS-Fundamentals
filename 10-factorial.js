7317f4d02c215e1d6d5a6c40b812cab250a1a09e
Galgallo Jaldesa <galgallosafaro5@gmail.com>
2025-11-09 12:13:42 +0300

    Add a script to compute and print a factorial

--- 10-factorial.js
+++ 10-factorial.js
@@ -2,13 +2,17 @@
 
 // Recursive function to compute factorial
 function factorial(n) {
-  if (n <= 1) return 1; // base case
-  return n * factorial(n - 1); // recursive case
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
 }
 
 // Get the first argument
 const args = process.argv.slice(2);
 const num = parseInt(args[0]);
 
-// Print factorial
-console.log(factorial(num));
+// If num is not a number, print 1
+if (isNaN(num)) {
+  console.log(1);
+} else {
+  console.log(factorial(num));
+}
