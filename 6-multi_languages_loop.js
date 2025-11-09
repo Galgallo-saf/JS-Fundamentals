14e7669428bd3e69e475e61337bc334fdd4f0145
Galgallo Jaldesa <galgallosafaro5@gmail.com>
2025-11-09 11:38:01 +0300

    Add script to print languages using a loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Create an array with the languages and messages
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Use a for loop to go through each item in the array
+for (let i = 0; i < languages.length; i++) {
+  // Print the current message
+  console.log(languages[i]);
+}
